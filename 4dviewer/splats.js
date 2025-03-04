ffe819a7942821130afd615777fd1d1e1ba9f35c
zhouhyOcean <zhouhaiyang000@gmail.com>
2025-03-05 00:06:33 +0800

    carouse

--- 4dviewer/splats.js
+++ 4dviewer/splats.js
@@ -108,7 +108,7 @@ export async function setSplatScene(name, view) {
 export async function setupCarousel(carousel) {
     //let files = await listFolders();
 
-    files = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09'];
+    let files = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09'];
     shuffleArray(files);
 
     const prototype = carousel.querySelector("#splat-carousel-prototype");
